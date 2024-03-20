f9e9555ba39dabc86ac8674ddac0b27e3b8452b7
Efosky13 <157364260+Efosky13@users.noreply.github.com>
2024-03-20 11:32:22 +0100

    feat:start app changed

--- src/app/components/input.jsx
+++ src/app/components/input.jsx
@@ -4,6 +4,7 @@ export default function Input({type,username,userid,place}) {
   return (
     <li>
     <input type={type} name={username} id={userid} placeholder={place} 
+    
     className='p-4 border-4 border-solid border-slate-900 rounded-md m-5 w-[50%]'/>
  </li>
  )
