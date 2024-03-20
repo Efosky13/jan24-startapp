a3f487dfd97ab7412950523b4e1af0509e0fcd1d
Efosky13 <157364260+Efosky13@users.noreply.github.com>
2024-03-20 20:16:17 +0100

    feat done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Pic from  './pic2 .jpg'
+import style from'./components/styles.modules.css'
 
 export default function Home() {
 
@@ -7,17 +8,14 @@ export default function Home() {
 
   return (
     <>
-    <div className={Style.home}>
+    <div className={style.home}>
       <div className="grid grid-cols-3">
 
 
      <Image src='/pic 1.jpg' alt="Pic of a girl" width='100' height='100'/>
-     <Image src={pic} alt="pic of man " width='100' height='100'/>
+     <Image src={Pic} alt="pic of man " width='100' height='100'/>
      <Image src='https://images.unsplash.com/photo-1459802071246-377c0346da93?q=80&w=1609&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'alt="water" width='100' height='100'/>
 
-
-
-        
 
       </div> 
 
